b23321b73e4ba3ef0ab2c0eeaf15248e313d1b7b
Junrapxng <6431501013@lamduan.mfu.ac.th>
2024-04-04 02:22:45 +0700

    import

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,27 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 
+// import Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+/* add icons to the library */
+library.add(fas, far, fab)
+
 const app = createApp(App)
 
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(createPinia())
 app.use(router)
 
